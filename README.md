95c5a4c7c30d738770438f72d38b910faf867b03
Quentin Bruyere <quentin.bruyere1@gmail.com>
2023-01-17 05:35:57 +0100

    Alpha Release
    - External JS file created
    - Commentary added
    - useless files deleted
    
    Missing:
    - Languages Card design

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # quentinbruyere.github.io
  Live-Test-Page before publishing works
+
+
+## Currently Working On
+CV in Glassmorphism styled with Tilt
---
+++ assets/js/script.js
@@ -0,0 +1,76 @@
+addEventListener('DOMContentLoaded', (event) => {
+
+
+    ////////// VARIABLES
+    //////
+    ///
+    let tiltButton = document.querySelector(".tiltButton");
+    let tiltCheckbox = document.querySelector(".tiltCheckbox");
+    let profilSection = document.querySelector(".profile-section");
+    let profilCard = document.querySelector(".profile-card");
+    let topCard = document.querySelector(".top-card");
+    let languages = document.querySelector(".languages");
+    let social = document.querySelector(".social");
+
+    var isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
+
+
+    ////////// INITIALISATION
+    //////
+    ///
+    scalling();
+
+
+    ////////// EVENT-LISTENERS
+    //////
+    ///
+    window.addEventListener("resize", scalling);
+    tiltCheckbox.addEventListener("click", tiltSwitch);
+
+
+    ////////// FUNCTIONS
+    //////
+    ///
+    function scalling(){
+        let profilCardHeight = profilCard.offsetHeight;
+        let scale = window.innerHeight/(profilCardHeight + 100);
+
+        profilSection.style.setProperty("transform", `scale(${scale})`);
+
+        // console.log("page scaled");
+    }
+
+    function tiltSwitch(){
+        if(tiltCheckbox.checked){
+            profilCard.setAttribute("data-tilt-full-page-listening", "");
+            VanillaTilt.init(profilCard, {
+                max: 5,
+                glare: true,
+                "max-glare": .3,
+            });
+            
+            if(isIOS){
+                DeviceMotionEvent.requestPermission();
+            }
+            // console.log("checkbox checked");
+        }
+        if(!tiltCheckbox.checked){
+            profilCard.vanillaTilt.destroy();
+            // console.log("should destroy");
+        }
+    }
+
+
+    ////////// MOBILE TRANSLATE
+    ////// Smaller device means smaller perspective
+    ////
+    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+        // true for mobile device
+        profilCard.setAttribute("data-tilt-perspective", "200");
+        tiltButton.style.setProperty("transform", "translateZ(12px)");
+        topCard.style.setProperty("transform", "translateZ(7px)");
+        languages.style.setProperty("transform", "translateZ(12px)");
+        social.style.setProperty("transform", "translateZ(7px)");
+    }
+
+});
